742977ece7f980c8b48706d6f73eebf69939a31b
ansrin <64233038+ansrin@users.noreply.github.com>
2020-10-19 15:12:15 -0700

    First Azure SDK "Hello World" sample service client generation tutorial

---
+++ sdk/api-learn/Azure.Learn.AppConfig/src/ConfigurationSetting.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Azure.Core;
+
+namespace Azure.Learn.AppConfig
+{
+    /// <summary>
+    /// ConfigurationSetting
+    /// </summary>
+    [CodeGenModel("KeyValue")]
+    public partial class ConfigurationSetting
+    {
+        /// <summary>
+        /// Etag
+        /// </summary>
+        [CodeGenMember("Etag")]
+        public ETag ETag { get; }
+    }
+}
